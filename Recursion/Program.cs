99ee1be4d09b4868a17cc59792def0eed546849e
Antikvarnyy <radomir.degterev2002@gmail.com>
2022-10-25 14:36:37 +0300

    Добавьте файлы проекта.

---
+++ Recursion/Program.cs
@@ -0,0 +1,21 @@
+﻿string path = "D:\\";
+Rekurs(path);
+void Rekurs(string path)
+{
+    if (Directory.Exists(path) && path != "D:\\System Volume Information")
+    {
+        Console.WriteLine("Файлы: ");
+        string[] files = Directory.GetFiles(path);
+        foreach (string file in files)
+        {
+            Console.WriteLine(file);
+        }
+        Console.WriteLine("Подкатолог:");
+        string[] dirs = Directory.GetDirectories(path);
+        foreach (string dir in dirs)
+        {
+            Console.WriteLine(dir);
+            Rekurs(dir);
+        }
+    }
+}
